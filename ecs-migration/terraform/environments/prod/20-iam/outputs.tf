3c4425fb6876f93c4384e03ab1e8f007d10d5044
Infra_JSJ <wwsdfv@gmail.com>
2026-01-14 03:03:41 +0900

    fix: IAM outputs에 sensitive 플래그 추가

--- ecs-migration/terraform/environments/prod/20-iam/outputs.tf
+++ ecs-migration/terraform/environments/prod/20-iam/outputs.tf
@@ -5,6 +5,7 @@
 output "task_execution_role_arn" {
   description = "ECS Task Execution Role ARN"
   value       = module.iam.task_execution_role_arn
+  sensitive   = true
 }
 
 output "task_execution_role_name" {
@@ -15,6 +16,7 @@ output "task_execution_role_name" {
 output "lgtm_task_role_arn" {
   description = "LGTM Task Role ARN (S3 access)"
   value       = module.iam.lgtm_task_role_arn
+  sensitive   = true
 }
 
 output "lgtm_task_role_name" {
@@ -25,6 +27,7 @@ output "lgtm_task_role_name" {
 output "alloy_task_role_arn" {
   description = "Alloy Task Role ARN (CloudWatch access)"
   value       = module.iam.alloy_task_role_arn
+  sensitive   = true
 }
 
 output "alloy_task_role_name" {
@@ -35,6 +38,7 @@ output "alloy_task_role_name" {
 output "grafana_task_role_arn" {
   description = "Grafana Task Role ARN"
   value       = module.iam.grafana_task_role_arn
+  sensitive   = true
 }
 
 output "grafana_task_role_name" {
